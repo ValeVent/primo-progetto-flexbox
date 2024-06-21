464e7671b8f3e6cb3cf7ff1a1dbe911096d64b4f
VALENTINA VENTURO <VALENTINAVENTURO90@GMAIL.COM>
2024-06-21 11:52:44 +0200

    migliorata versione desktop, gestite media queries per tablet (con hamburger menu)

---
+++ funcions.js
@@ -0,0 +1,24 @@
+// functions.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    var menuToggle = document.getElementById("menu-toggle");
+    var mobileMenu = document.getElementById("mobile-menu");
+    var menuItems = mobileMenu.getElementsByClassName("menu-item");
+
+    menuToggle.addEventListener("click", function() {
+        if (mobileMenu.style.display === "none" || mobileMenu.style.display === "") {
+            mobileMenu.style.display = "flex";
+            menuToggle.textContent = "Menu -";
+        } else {
+            mobileMenu.style.display = "none";
+            menuToggle.textContent = "Menu +";
+        }
+    });
+
+    for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener("click", function() {
+            mobileMenu.style.display = "none";
+            menuToggle.textContent = "Menu +";
+        });
+    }
+});
